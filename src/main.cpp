200f21398c0f5a71100be2d253737aeeed2a2b90
hsuchy <johnny11118@gmail.com>
2024-12-21 18:25:09 +0800

    [fix] make parse time is print at first line, make auto run script easier to collect runtime

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 int main(int argc, char **argv) {
     using namespace std::chrono;
 
-    time_point<high_resolution_clock> startTime = high_resolution_clock::now();
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
@@ -39,7 +38,8 @@ int main(int argc, char **argv) {
     readFileContents(input_file, threadContext);
     int contentSize = threadContext->getContents().size();
 
-    time_point<high_resolution_clock> inputTime = high_resolution_clock::now();
+    // time_point<high_resolution_clock> inputTime = high_resolution_clock::now();
+    time_point<high_resolution_clock> startTime = high_resolution_clock::now();
 
     for (int i = 1; i < threadCount; i++) {
       int start = i * contentSize / threadCount;
@@ -47,23 +47,28 @@ int main(int argc, char **argv) {
       threadManager.add_thread(parse_content, i, start, end, threadContext, mode, &threadManager);
     }
     parse_content(0, 0, contentSize / threadCount, threadContext, mode, &threadManager);
-    threadManager.joinAll();
+    if (threadCount > 1) {
+      threadManager.joinAll();
+    }
 
     time_point<high_resolution_clock> parseTime = high_resolution_clock::now();
 
-    threadContext->getRecord(0).print_summary(false);
+    duration<double> totalDuration = (parseTime - startTime);
+    std::cout << totalDuration.count() << std::endl;
+
+    threadContext->getRecord(0).print_summary(true);
     input_file.close();
 
-    time_point<high_resolution_clock> outputTime = high_resolution_clock::now();
+    // time_point<high_resolution_clock> outputTime = high_resolution_clock::now();
 
-    duration<double> inputDuration = (inputTime - startTime);
-    duration<double> parseDuration = (parseTime - inputTime);
-    duration<double> outputDuration = (outputTime - parseTime);
-    duration<double> totalDuration = (outputTime - startTime);
-
-    std::cout << "inputDuration = " << inputDuration << std::endl;
-    std::cout << "parseDuration = " << parseDuration << std::endl;
-    std::cout << "outputDuration = " << outputDuration << std::endl;
-    std::cout << "totalDuration = " << totalDuration << std::endl;
+    // duration<double> inputDuration = (inputTime - startTime);
+    // duration<double> parseDuration = (parseTime - inputTime);
+    // duration<double> outputDuration = (outputTime - parseTime);
+    // duration<double> totalDuration = (outputTime - startTime);
+    //
+    // std::cout << "inputDuration = " << inputDuration << std::endl;
+    // std::cout << "parseDuration = " << parseDuration << std::endl;
+    // std::cout << "outputDuration = " << outputDuration << std::endl;
+    // std::cout << "totalDuration = " << totalDuration << std::endl;
     return 0;
 }
